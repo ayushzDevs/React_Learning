9cbc767b847b9fe5785e840bfc60cec954dc4d62
ayushzAIML <theayushchakraborty@gmail.com>
2026-05-28 19:41:32 +0530

    fr

---
+++ Basic_React_App/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css"
+
+
+
+function Title(){
+  return <h1>My First React App</h1>
+}
+function Des(){
+  return <h3>I am description</h3>
+}
+function App() {
+  return (
+    <div>
+      <Title/>
+      <Des/>
+    </div>
+  )
+}
+
+export default App
+ 
+ 
