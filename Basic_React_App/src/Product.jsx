ae03ad3286b94d320b9ca05402da27db853e3232
ayushzAIML <theayushchakraborty@gmail.com>
2026-05-28 20:28:51 +0530

    fr

--- Basic_React_App/src/Product.jsx
+++ Basic_React_App/src/Product.jsx
@@ -1,13 +1,15 @@
+import "./Product.css"
+
 function Product(){
     return(
-        <>
+        <div className="Product">
 
             <h3>Product Title</h3>
             <h5>Product description</h5>
         
         
         
-        </>
+        </div>
     )
 }
 
