05c569d05fd883bce6ba3451c0bf6271309a0348
ayushzAIML <theayushchakraborty@gmail.com>
2026-06-01 20:11:35 +0530

    1 june

--- React_States/src/likebutton.jsx
+++ React_States/src/likebutton.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 function LikeButton(){
+    
+    let [isLiked , setisLiked] = useState(false)
+
+    let toggleLike = () =>{
+        setisLiked(!isLiked);
+
+    }
+
+
     return(
         <div>
-
-            <p>Like Button</p>
-            
-
+           <p onClick={toggleLike} >
+            {
+                isLiked ? (
+                <i className="fa-solid fa-heart"></i>
+            ): (
+                <i className="fa-regular fa-heart"></i>)
+            }
+           </p>
         </div>
         
     )
