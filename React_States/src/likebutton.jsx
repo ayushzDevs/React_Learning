6f45c95e8e887a0e3fea80cc09be1317f75c4a58
ayushzAIML <theayushchakraborty@gmail.com>
2026-06-01 20:14:28 +0530

    1 june

--- React_States/src/likebutton.jsx
+++ React_States/src/likebutton.jsx
@@ -7,15 +7,16 @@ function LikeButton(){
     let toggleLike = () =>{
         setisLiked(!isLiked);
 
-    }
+    };
 
+    let likeStyle = {color : "red"};
 
     return(
         <div>
            <p onClick={toggleLike} >
             {
                 isLiked ? (
-                <i className="fa-solid fa-heart"></i>
+                <i className="fa-solid fa-heart" style={likeStyle}></i>
             ): (
                 <i className="fa-regular fa-heart"></i>)
             }
@@ -23,6 +24,6 @@ function LikeButton(){
         </div>
         
     )
-}
+};
 
 export default LikeButton;
