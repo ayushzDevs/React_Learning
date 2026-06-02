0979697eb73ab04d98c6333cef27d6745b8a8e0d
ayushzAIML <theayushchakraborty@gmail.com>
2026-06-02 16:58:24 +0530

    2 june

--- React_States/src/Counter.jsx
+++ React_States/src/Counter.jsx
@@ -2,26 +2,29 @@ import React from 'react'
 import { useState } from 'react';
 
 
+function init(){
+  return Math.random();
+}
 
 
 function Counter() {
-    let [Count, setCount] = useState(0)
+    let [Count, setCount] = useState(init())
     console.log("compoonent is rendered");
     console
 
     let incCount = ()=>{
 
-        // setCount((currVal)=>{
-        //   return currVal +1
-        // });
-        // setCount((currVal)=>{
-        //   return currVal +1
+        setCount((currVal)=>{
+          return currVal +1
+        });
+        setCount((currVal)=>{
+          return currVal +1
 
-        // });
-        // setCount((currVal)=>{
-        //   return currVal +1
+        });
+        setCount((currVal)=>{
+          return currVal +1
 
-        // });
+        });
 
         // setCount(25)
         // console.log(Count)
