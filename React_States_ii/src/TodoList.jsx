27ca1cb19e10fd49fdba19fb4ca0510a714147a1
ayushzAIML <theayushchakraborty@gmail.com>
2026-06-02 21:08:52 +0530

    2 june

--- React_States_ii/src/TodoList.jsx
+++ React_States_ii/src/TodoList.jsx
@@ -5,22 +5,34 @@ import {v4 as uuidv4} from "uuid";
 function TodoList() {
 
 
-  let [Todo,setTodo] = useState({task:"sample",id:uuidv4()})
+  let [Todo,setTodo] = useState([{todo:"sample",id:uuidv4()}])
   const [task,setTask] =useState("")
 
-  let updateTodo = ()=>{
+  let addnewTask = ()=>{
     setTodo((prevTodo)=>{
-      return[...prevTodo,task]
+      return[...prevTodo,{todo: task, id:uuidv4()}]
     })
+    setTask("")
   }
+
+  let updateTodoValue = (e)=>{
+    setTask(e.target.value);
+  }
+
+
+  let deleteTodo=(id)=>{
+
+    setTodo(Todo.filter((t)=> t.id != id));
+
+  }
+
 
   return (
     <div>
-        <input placeholder='add a task' value={task} onChange={(e)=>{
-          setTask(e.target.value)
-        }}></input>
+        <input placeholder='add a task' value={task} onChange={updateTodoValue
+        }></input>
         <br />
-        <button onClick={updateTodo}>Add Task</button>
+        <button onClick={addnewTask}>Add Task</button>
         <br /><br /><br /><br />
 
 
@@ -29,11 +41,17 @@ function TodoList() {
         <h4>Todo List</h4>
         <ul>
             {
-
+              Todo.map((t)=>{
+                return (
+                <li key={t.id}>
+                  <span>{t.todo}</span>
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  <button onClick={()=>deleteTodo(t.id)}>Delete</button></li>)
+              })
             }
         </ul>
     </div>
   )
 }
 
-export default TodoList;
+export default TodoList
