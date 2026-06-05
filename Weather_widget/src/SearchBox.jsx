f774a90888ff07a5c860026fdc398840962f5e71
ayushzAIML <theayushchakraborty@gmail.com>
2026-06-05 20:56:00 +0530

    Weather Widget

--- Weather_widget/src/SearchBox.jsx
+++ Weather_widget/src/SearchBox.jsx
@@ -6,6 +6,32 @@ import "./SearchBox.css"
 
 
 function SearchBox() {
+
+  
+
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather"
+  const API_KEY = "9f5a90df6eabf396462e4c9e1babe381"
+
+
+  
+
+  let getWeatherInfo = async()=>{
+    let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`)
+    let jsonResponse = await response.json()
+    // console.log(jsonResponse)
+
+    let res = {
+      temp: jsonResponse.main.temp,
+      humidity: jsonResponse.main.humidity,
+      pressure: jsonResponse.main.pressure,
+      weatherMood: jsonResponse.weather[0].main,
+      name: jsonResponse.name,
+      speed: jsonResponse.wind.speed,
+      country: jsonResponse.sys.country
+    }
+    console.log(res);
+  }
+
   let [city , setCity] = useState("")
 
   let handleChange = (e) =>{
@@ -14,8 +40,9 @@ function SearchBox() {
 
   let handleSubmit = (e)=>{
     e.preventDefault();
-    console.log(city)
+    
     setCity("")
+    getWeatherInfo();
   }
 
   return (
