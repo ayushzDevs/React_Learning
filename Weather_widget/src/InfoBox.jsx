47cea52dd11f36bd8448dc532319b4b3ff5fa03c
ayushzAIML <theayushchakraborty@gmail.com>
2026-06-06 12:31:33 +0530

    Weather Widget

--- Weather_widget/src/InfoBox.jsx
+++ Weather_widget/src/InfoBox.jsx
@@ -9,15 +9,17 @@ import "./InfoBox.css"
 
 function InfoBox({ info }) {
 
-    const INIT_URL = "https://plus.unsplash.com/premium_vector-1722006399427-8b81a53dbe7f?q=80&w=2040&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-
+    const INIT_URL = "https://images.unsplash.com/vector-1775847261677-3a68249f433a?q=80&w=789&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    const HOT_URL="https://plus.unsplash.com/premium_vector-1721216582924-6eeccd62eb24?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    const COLD_URL="https://plus.unsplash.com/premium_vector-1728551180293-92d0f794988a?q=80&w=747&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    const RAINY_URL="https://images.unsplash.com/vector-1747330794373-9210743903c3?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   return (
     <div className='InfoBox'>
         <div className='CardContainer'>
         <Card sx={{ maxWidth: 500 }}>
             <CardMedia
                 sx={{ height: 140, width: 500 }}
-                image={INIT_URL}
+                image={info.humidity>80 ? RAINY_URL : info.temp>26 ? HOT_URL:COLD_URL}
                 title="green iguana"
             />
             <CardContent>
@@ -35,7 +37,7 @@ function InfoBox({ info }) {
                     <br />
                     Temperature : {info.temp}  
                     <br /> 
-                    Feelslike : {info.feels_like}   
+                    Feelslike : {info.feelslike}   
                 </Typography>
             </CardContent>
             <CardActions>
--- Weather_widget/src/SearchBox.jsx
+++ Weather_widget/src/SearchBox.jsx
@@ -6,7 +6,8 @@ import "./SearchBox.css"
 import WeatherApp from './WeatherApp';
 
 function SearchBox({updateInfo}) {
-
+    let [city , setCity] = useState("")
+    let [error, setError] = useState(false)
   
 
   const API_URL = "https://api.openweathermap.org/data/2.5/weather"
@@ -16,6 +17,7 @@ function SearchBox({updateInfo}) {
   
 
   let getWeatherInfo = async()=>{
+    try{
     let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`)
     let jsonResponse = await response.json()
     console.log(jsonResponse)
@@ -32,19 +34,29 @@ function SearchBox({updateInfo}) {
     }
     return res;;
   }
+    catch(error)
+    {
+      throw error
+    }
+  }
 
-  let [city , setCity] = useState("")
 
   let handleChange = (e) =>{
+
     setCity(e.target.value)
   }
 
   let handleSubmit = async(e)=>{
-    e.preventDefault();
-    setCity("")
-    let newInfo =await getWeatherInfo();
-    updateInfo(newInfo)
-  }
+        try{
+            e.preventDefault();
+            setCity("")
+            let newInfo =await getWeatherInfo();
+            updateInfo(newInfo)
+        }
+          catch(e){
+            setError(true)
+          }
+        }
 
   return (
     <div className='SearchBox'>
@@ -53,6 +65,7 @@ function SearchBox({updateInfo}) {
               <br /><br />
               <Button variant='contained' type='submit' >Search</Button>
               <br /><br /><br /><br />
+              {error && <p style={{ color: 'red' }}>City Not Found</p>}
         </form>
     </div>
   )
